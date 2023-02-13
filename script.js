277aaf738dd2456dfeef4768694cd74b6cd83467
lefrodrigues <97368754+lefrodrigues@users.noreply.github.com>
2023-02-13 17:25:45 -0300

    correction bug validate

--- script.js
+++ script.js
@@ -4,31 +4,39 @@ const submit = document.getElementById("button_sigin");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-  if (email.value == "") {
-    email.classList.add("errorInput");
-  } else {
-    email.classList.remove("errorInput");
+
+  // verifica se o email foi preenchido e se é válido
+  if (email.value == "" || !isEmailValid(email.value)) {
+    alert("O campo email é obrigatório, digite um email válido");
+    return;
   }
 
-  if (password.value == "") {
-    password.classList.add("errorInput");
-  } else {
-    password.classList.remove("errorInput");
+  // verifica se a senha foi preenchido e se é válida
+
+  if (!validatePassword(password.value, 6)) {
+    alert("A senha deve ter no mínimo 6 dígitos");
   }
 
-  if (
-    email.value.indexOf("@") == -1 ||
-    email.value.indexOf(".") == -1 ||
-    email.value.indexOf(".") - email.value.indexOf("@") == 1
-  ) {
-    email.classList.add("errorInput");
-  } else {
-    email.classList.remove("errorInput");
-  }
-
-  if (password.value.lenght <= 5) {
-    password.classList.add("errorInput");
-  } else {
-    password.classList.remove("errorInput");
-  }
+  form.submit();
 });
+
+// Função que valida email
+function isEmailValid(email) {
+  const emailRegex = new RegExp(
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+  );
+
+  if (emailRegex.test(email)) {
+    return true;
+  }
+  return false;
+}
+
+// Função que valida a senha
+function validatePassword(password, minDigits) {
+  if (password.length >= minDigits) {
+    //senha válida
+    return true;
+  }
+  return false;
+}
